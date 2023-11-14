8e3c175d46112edeeaf9a45bc9127addd8127dd1
clementbrousseau <127963131+clementbrousseau@users.noreply.github.com>
2023-11-14 17:09:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from partie import Partie
-from grille import Grille
-from case import Case
-from etat_case import EtatCase
- 
+from partie2 import Partie
+
 if __name__ == "__main__":
     partie = Partie()
     partie.choisir_difficulte("facile")
     partie.generer_grille()
     partie.jouer()
-    
